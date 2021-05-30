82966931c49d41bab26189708886f456c02d6b1c
dev-abir <gangulyabir777@gmail.com>
2021-05-30 15:01:21 +0530

    now there can be separate js, css files, and some improvements

--- Room.py
+++ Room.py
@@ -4,7 +4,7 @@ from Game import Game
 
 
 class Player:
-    def __init__(self, uid, uname, game_character, room_id, ws_sid):
+    def __init__(self, uid, uname, game_character, ws_sid, room_id=None):
         self.uid = uid
         self.uname = uname
         self.game_character = game_character
@@ -12,14 +12,15 @@ class Player:
         self.ws_sid = ws_sid  # websocket sid(for sending messages to a particular player)
 
     def to_dict(self):
-        return {"uid": self.uid, "uname": self.uname, "game_character": self.game_character, "room_id": self.room_id, "ws_sid": self.ws_sid}
+        return {"uid": self.uid, "uname": self.uname, "game_character": self.game_character, "room_id": self.room_id,
+                "ws_sid": self.ws_sid}
 
     def __str__(self):
         return str(self.to_dict())
 
 
 class Room:
-    def __init__(self, room_id, player1, player2):
+    def __init__(self, room_id, player1=None, player2=None):
         self.room_id = room_id
         self.player1 = player1
         self.player2 = player2
@@ -28,14 +29,16 @@ class Room:
     def broadcast_board(self):
         emit("get_board", self.game.board, to=self.player1.ws_sid)
         emit("get_board", self.game.board, to=self.player2.ws_sid)
-    
+
     def broadcast_event(self, event, message):
         emit(event, message, to=self.player1.ws_sid)
         emit(event, message, to=self.player2.ws_sid)
 
     def broadcast_player_data(self):
-        emit("player_data", self.player1.to_dict(), to=self.player1.ws_sid)
-        emit("player_data", self.player2.to_dict(), to=self.player2.ws_sid)
+        if self.player1 is not None:
+            emit("player_data", self.player1.to_dict(), to=self.player1.ws_sid)
+        if self.player2 is not None:
+            emit("player_data", self.player2.to_dict(), to=self.player2.ws_sid)
 
     def add_player(self, new_player):
         if self._add_player(new_player):
