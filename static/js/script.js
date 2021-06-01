778c1b40c91215cdfba612986eb28fe34c69e58d
dev-abir <gangulyabir777@gmail.com>
2021-06-01 09:48:13 +0530

    bugfix, small improvements

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,16 @@
 var socket = io();
 var player_data;
 
+// TODO: these should be const, but it gives error...
+var CIRCLE_IMG_URL = "https://pic.onlinewebfonts.com/svg/img_416983.png";
+var CROSS_IMG_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Red_X.svg/1200px-Red_X.svg.png";
+
+// preload images
+var img0 = new Image();
+img0.setAttribute('src', CIRCLE_IMG_URL);
+var img1 = new Image();
+img1.setAttribute('src', CROSS_IMG_URL);
+
 socket.on('connect', function () {
 	socket.emit('join', { uname: new Date().toLocaleTimeString() }, (res) => {
 		console.log('ok');
@@ -12,9 +22,7 @@ socket.on("message", function (message) {
 });
 
 socket.on("get_board", function (board_data) {
-	var img = document.createElement('img');
 	console.log(board_data);
-
 	var x = 0;
 	var charCodeA = 'A'.charCodeAt(0);
 	for (var i = 0; i < board_data.length; ++i) {
@@ -22,17 +30,21 @@ socket.on("get_board", function (board_data) {
 			var targetElement = document.getElementById('square' + String.fromCharCode(charCodeA + x));
 
 			if (board_data[i][j] == 'o') {
-				img.setAttribute('class', 'game_circle');
-				img.setAttribute('src', "https://pic.onlinewebfonts.com/svg/img_416983.png");
-				if (!targetElement.firstChild) // if there's no symbol in the square
-					targetElement.appendChild(img);
-				console.log(board_data[i][j], i, j, x, 'square' + String.fromCharCode(charCodeA + x));
+				if (!targetElement.firstChild) { // if there's no symbol in the square
+					 // create new object, recycling old ones can cause problems...
+					var circleImg = document.createElement("img");
+					circleImg.setAttribute('class', 'game_circle');
+					circleImg.setAttribute('src', CIRCLE_IMG_URL);
+					targetElement.appendChild(circleImg);
+				}
 			} else if (board_data[i][j] == 'x') {
-				img.setAttribute('class', 'game_cross');
-				img.setAttribute('src', "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Red_X.svg/1200px-Red_X.svg.png");
-				if (!targetElement.firstChild) // if there's no symbol in the square
-					targetElement.appendChild(img);
-				console.log(board_data[i][j], i, j, x, 'square' + String.fromCharCode(charCodeA + x));
+				if (!targetElement.firstChild) { // if there's no symbol in the square
+					// create new object, recycling old ones can cause problems...
+					var crossImg = document.createElement("img");
+					crossImg.setAttribute('class', 'game_cross');
+					crossImg.setAttribute('src', CROSS_IMG_URL);
+					targetElement.appendChild(crossImg);
+				}
 			} else {
 				// for '-', clear the square...
 				while (targetElement.firstChild) {
@@ -49,8 +61,23 @@ socket.on("player_data", function (data) {
 	console.log("player char", player_data.game_character);
 });
 
+socket.on("game_draw", function(message) {
+	console.log(message);
+	console.log("refreshing page within 3 seconds for a new game...");
+	setTimeout(function () {
+		window.location.reload();
+	}, 3 * 1000);
+});
+
+socket.on("game_won", function(message) {
+	console.log(message);
+	console.log("refreshing page within 3 seconds for a new game...");
+	setTimeout(function () {
+		window.location.reload();
+	}, 3 * 1000);
+});
+
 function fire(event, posX, posY) {
 	console.log("turn", posX, posY);
 	socket.emit("turn", { "posX": posX, "posY": posY });
 }
-// TODO: also it has game_draw, game_won events.......
